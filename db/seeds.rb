f5d28515139f890c188dd0bbca7575ed847deac6
Marco Ricci <mr.ricci79@gmail.com>
2022-06-07 17:01:33 +0100

    start filter search

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,6 +38,7 @@ flat2 = Flat.new(name: "Red House",
                   bedrooms: 2,
                   beds: 3,
                   price: 300,
+                  wifi: true,
                   user: user2,
                   description: "Beautiful, bright two bedroom bungalow moments from the beach. Perfect for short and long stays. All essential amenities, log burner, garden set and welcome gift and guide included. Exclusive use of full property.")
 
@@ -47,5 +48,25 @@ file = URI.open("https://imganuncios.mitula.net/maisonette_in_churton_place_piml
 flat2.photos.attach(io: file, filename: 'red2.jpg', content_type: 'image/jpg')
 flat2.save!
 puts "flat #{flat2.id} created"
+
+flat3 = Flat.new(name: "Casa Mare",
+                  guests: 8,
+                  country: "italy",
+                  city: "Portofino",
+                  address: "Via Duca degli Abruzzi, 31, Portofino",
+                  bedrooms: 2,
+                  beds: 6,
+                  price: 2500,
+                  wifi: true,
+                  user: user2,
+                  description: "Bright and spacious family or holiday home just a short walk from the beach, shopping, and restaurants, nicely positioned at the rear of the desirable gated community of Altos de Flamingo.")
+
+file = URI.open("https://lid.zoocdn.com/645/430/d3208cc41692eff8cdd294d10756795b7fa68a17.jpg")
+flat3.photos.attach(io: file, filename: 'portofino.jpg', content_type: 'image/jpg')
+file = URI.open("https://lid.zoocdn.com/645/430/5c6f8fcaef9a72130df4ffd93b6aa6155314f433.jpg")
+flat3.photos.attach(io: file, filename: 'portofino.jpg', content_type: 'image/jpg')
+flat3.save!
+puts "flat #{flat3.id} created"
+
 
 puts "Done!"
