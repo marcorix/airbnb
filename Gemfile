9da4efaf4f36590db4f2544b70380e361ebb474e
Marco Ricci <mr.ricci79@gmail.com>
2025-01-30 13:00:46 +0100

    chat feature no css

--- Gemfile
+++ Gemfile
@@ -57,6 +57,8 @@ gem "sassc-rails"
 
 gem "cloudinary"
 
+gem "solid_cable"
+
 group :development, :test do
   gem "dotenv-rails"
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,15 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :booking
-  
+
+  after_create_commit :broadcast_message
+
+  private
+
+  def broadcast_message
+    broadcast_append_to "booking_#{booking.id}_messages",
+                        partial: "messages/message",
+                        target: "messages",
+                        locals: { message: self }
+  end
 end
