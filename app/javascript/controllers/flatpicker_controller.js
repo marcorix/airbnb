b23f7dea473f1e9b57da6dacdc3923c1fadad32b
Marco Ricci <mr.ricci79@gmail.com>
2025-02-25 16:09:38 +0100

    js for total price

--- app/javascript/controllers/flatpicker_controller.js
+++ app/javascript/controllers/flatpicker_controller.js
@@ -3,10 +3,14 @@ import flatpickr from "flatpickr";
 
 // Connects to data-controller="flatpicker"
 export default class extends Controller {
-  static targets = ['start', 'end', 'checkIn']
+  static targets = ['start', 'end', 'checkIn', 'total', 'days']
+  static values = {
+    price: Number
+  }
 
   connect() {
     console.log("Hello, Stimulus!", this.element)
+    console.log("Price:", this.priceValue)
     flatpickr(this.checkInTarget, {
       mode: "range",
       onChange: ([start, end]) => {
@@ -14,7 +18,18 @@ export default class extends Controller {
           this.startTarget.value = start
           this.endTarget.value = end
         }
+        this.calculateDays()
       }
     })
+  }
+
+  calculateDays() {
+    const start = new Date(this.startTarget.value);
+    const end = new Date(this.endTarget.value);
+    const diffTime = Math.abs(end - start);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    const totalPrice = diffDays * this.priceValue
+    this.totalTarget.innerText = totalPrice.toFixed(2);
+    this.daysTarget.innerText = diffDays;
   }
 }
