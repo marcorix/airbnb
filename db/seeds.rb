373e6ec6022c2a5ef2947b0122061ebadb8d4027
Marco Ricci <mr.ricci79@gmail.com>
2025-02-25 15:38:00 +0100

    increased the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,50 +37,70 @@ flat1.photos.attach(io: file5, filename: "nes.png", content_type: "image/png")
 flat1.save!
 puts "Flat #{Flat.count}"
 
-# flat2 = Flat.new(
-#   name: "Skysreaper view",
-#   address: "Stratford, London",
-#   guests: 6,
-#   price: 440,
-#   property_type: "flat",
-#   description: Faker::Lorem.sentence(word_count: 20),
-#   user: user2
-# )
-# file = URI.open("https://images.unsplash.com/photo-1619460485417-82881a4c984d?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
-# # file2 = URI.open("https://images.unsplash.com/photo-1613775967978-d07fb817ce9a?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
-# # file3 = URI.open("https://images.unsplash.com/photo-1640872162491-776671bd7d79?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
-# flat2.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
-# # flat2.photos.attach(io: file2, filename: "nes.png", content_type: "image/png")
-# # flat2.photos.attach(io: file3, filename: "nes.png", content_type: "image/png")
-# flat2.save!
-# puts "Flat #{Flat.count}"
+flat2 = Flat.new(
+  name: "Skysreaper view",
+  address: "Stratford, London",
+  guests: 6,
+  price: 440,
+  property_type: "flat",
+  description: Faker::Lorem.sentence(word_count: 20),
+  user: user2
+)
+file1 = URI.open("https://media.rightmove.co.uk/63k/62080/152041031/62080_UK-S-42432_IMG_10_0000.jpeg")
+file2 = URI.open("https://media.rightmove.co.uk/63k/62080/152041031/62080_UK-S-42432_IMG_05_0000.jpeg")
+file3 = URI.open("https://media.rightmove.co.uk/63k/62080/152041031/62080_UK-S-42432_IMG_04_0000.jpeg")
+file4 = URI.open("https://media.rightmove.co.uk/63k/62080/152041031/62080_UK-S-42432_IMG_00_0000.jpeg")
+file5 = URI.open("https://media.rightmove.co.uk/63k/62080/152041031/62080_UK-S-42432_IMG_15_0000.jpeg")
+flat2.photos.attach(io: file1, filename: "nes.png", content_type: "image/png")
+flat2.photos.attach(io: file2, filename: "nes.png", content_type: "image/png")
+flat2.photos.attach(io: file3, filename: "nes.png", content_type: "image/png")
+flat2.photos.attach(io: file4, filename: "nes.png", content_type: "image/png")
+flat2.photos.attach(io: file5, filename: "nes.png", content_type: "image/png")
+flat2.save!
+puts "Flat #{Flat.count}"
 
-# flat3 = Flat.new(
-#   name: "Travelodge House",
-#   address: "Soho, London",
-#   guests: 8,
-#   price: 370,
-#   property_type: "maisonette",
-#   description: Faker::Lorem.sentence(word_count: 20),
-#   user: user3
-# )
-# file = URI.open("https://www.buildington.co.uk/images/projects/1721669518669e978e646361.34063429.jpg")
-# flat3.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
-# flat3.save!
-# puts "Flat #{Flat.count}"
+flat3 = Flat.new(
+  name: "Travelodge House",
+  address: "Soho, London",
+  guests: 8,
+  price: 370,
+  property_type: "maisonette",
+  description: Faker::Lorem.sentence(word_count: 20),
+  user: user3
+)
+file1 = URI.open("https://media.rightmove.co.uk/50k/49602/152084636/49602_MAS210029_IMG_00_0000.jpeg")
+file2 = URI.open("https://media.rightmove.co.uk/50k/49602/152084636/49602_MAS210029_IMG_01_0000.jpeg")
+file3 = URI.open("https://media.rightmove.co.uk/50k/49602/152084636/49602_MAS210029_IMG_02_0000.jpeg")
+file4 = URI.open("https://media.rightmove.co.uk/50k/49602/152084636/49602_MAS210029_IMG_03_0000.jpeg")
+file5 = URI.open("https://media.rightmove.co.uk/50k/49602/152084636/49602_MAS210029_IMG_05_0000.jpeg")
+flat3.photos.attach(io: file1, filename: "nes.png", content_type: "image/png")
+flat3.photos.attach(io: file2, filename: "nes.png", content_type: "image/png")
+flat3.photos.attach(io: file3, filename: "nes.png", content_type: "image/png")
+flat3.photos.attach(io: file4, filename: "nes.png", content_type: "image/png")
+flat3.photos.attach(io: file5, filename: "nes.png", content_type: "image/png")
+flat3.save!
+puts "Flat #{Flat.count}"
 
-# flat4 = Flat.new(
-#   name: "Palazzo Manfredi",
-#   address: "Via Labicana, 125, Roma",
-#   guests: 6,
-#   price: 570,
-#   property_type: "rooftop",
-#   description: Faker::Lorem.sentence(word_count: 20),
-#   user: user1
-# )
-# file = URI.open("https://images.trvl-media.com/lodging/2000000/1070000/1065700/1065602/fa75df6a.jpg")
-# flat4.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
-# flat4.save!
-# puts "Flat #{Flat.count}"
+flat4 = Flat.new(
+  name: "Palazzo Manfredi",
+  address: "Via Labicana, 125, Roma",
+  guests: 6,
+  price: 570,
+  property_type: "rooftop",
+  description: Faker::Lorem.sentence(word_count: 20),
+  user: user1
+)
+file1 = URI.open("https://a0.muscache.com/im/pictures/639fed94-fe84-45af-a2cc-a250ba3a4f7e.jpg?im_w=1200&im_format=avif&im_origin=fuzzy")
+file2 = URI.open("https://a0.muscache.com/im/pictures/339b9f73-3981-4ae9-ab78-b20b51f37208.jpg?im_w=1440&im_format=avif&im_origin=fuzzy")
+file3 = URI.open("https://a0.muscache.com/im/pictures/c5f3a9d5-d684-42fb-a06c-e1197ef08be1.jpg?im_w=1440&im_format=avif&im_origin=fuzzy")
+file4 = URI.open("https://a0.muscache.com/im/pictures/53dd922c-9ae6-4bb5-a505-4fe9cab8d5be.jpg?im_w=1440&im_format=avif&im_origin=fuzzy")
+file5 = URI.open("https://a0.muscache.com/im/pictures/3faecd19-234e-407f-b89e-bf9a5f29bb2c.jpg?im_w=1440&im_format=avif&im_origin=fuzzy")
+flat4.photos.attach(io: file1, filename: "nes.png", content_type: "image/png")
+flat4.photos.attach(io: file2, filename: "nes.png", content_type: "image/png")
+flat4.photos.attach(io: file3, filename: "nes.png", content_type: "image/png")
+flat4.photos.attach(io: file4, filename: "nes.png", content_type: "image/png")
+flat4.photos.attach(io: file5, filename: "nes.png", content_type: "image/png")
+flat4.save!
+puts "Flat #{Flat.count}"
 
 puts "Created #{Flat.count} flats"
